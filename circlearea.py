9af99dd696fcb46270cde0b22539cefc00b233e4
OKeerthiKuttan <124677994+OKeerthiKuttan@users.noreply.github.com>
2023-04-10 20:17:33 +0530

    Update circlearea.py

--- circlearea.py
+++ circlearea.py
@@ -1,4 +1,12 @@
+#1
 import math
 r=float(input("enter the radius"))
 area=math.pi*r*r
 print("the area of the circle is",area)
+
+
+
+#2
+filename=input("enter file name")
+f=filename.split(".")
+print("the extension of the file is:", f[-1])
